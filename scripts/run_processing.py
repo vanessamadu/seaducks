1efcef72f75fc5c4a593894fddad6071d86632ba
Vanessa Madu <vm2218@ic.ac.uk>
2024-08-06 15:46:42 +0100

    working updates

--- scripts/run_processing.py
+++ scripts/run_processing.py
@@ -17,9 +17,9 @@ def main():
     region = iho_region_geometry(iho_file_path,iho_region)
     herald(f'region: {iho_region} created successfully')
     file_path = os.path.join('data', 'corrected_velocity_drifter_full.h5')
-    output_path = os.path.join('data', 'filtered_data_test.h5')
+    output_path = os.path.join('data', 'filtered_nao_drifter_full.h5')
     
-    data_filtering(region, file_path, output_path,sample_proportion=0.0001)
+    data_filtering(region, file_path, output_path)
 
 if __name__ == '__main__':
     main()
