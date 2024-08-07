ee01e4feae7bc9367db738ad7c7831826345affb
Vanessa Madu <vm2218@ic.ac.uk>
2024-08-07 12:29:07 +0100

    add heralds

--- seaducks/data_processing/derived_quantities.py
+++ seaducks/data_processing/derived_quantities.py
@@ -19,22 +19,33 @@ def sst_gradient(SST_array: xr.DataArray) -> tuple:
     SST_grad_array_y = xr.zeros_like(SST_array)
 
     for tt in t_array:
+        herald_count_ii = 0
         for ii in lat:
             sst_at_lat = SST_array.sel(latitude = ii,time=tt)
             SST_grad_array_x.loc[dict(time=tt, latitude=ii)] = diff1d(sst_at_lat.values, h)
-            print(f"lat:{ii.values} complete")
-        
+            # print and log progress
+            if herald_count_ii%50 == 0:
+                herald(f"lat: {herald_count_ii*50/len(lat)}% complete")
+                herald_count_ii +=1
+
+        herald_count_jj = 0
         for jj in lon:
             sst_at_lon = SST_array.sel(longitude = jj,time=tt)
-            SST_grad_array_y[tt,:,jj] = diff1d(sst_at_lon,h)
-            print(f"lon:{lon[jj]} complete")
+            SST_grad_array_y.loc[dict(time=tt, longitude=jj)] = diff1d(sst_at_lon.values, h)
+            # print and log progress
+            if herald_count_jj%50 == 0:
+                herald(f"lat: {herald_count_jj*50/len(lon)}% complete")
+                herald_count_jj +=1
+
+        herald(f"time: {tt} complete")
     return SST_grad_array_x,SST_grad_array_y
 
 def sst_gradient_to_da(sst,output_directory,output_filename):
 
     sst_gradient_x, sst_gradient_y = sst_gradient(sst)
+    herald("sst gradients computed successfully")
 
-    # create data arrays
+    # rename data arrays
     sst_gradient_x.name='sst_gradient_x'
     sst_gradient_y.name='sst_gradient_y'
 
@@ -45,7 +56,7 @@ def sst_gradient_to_da(sst,output_directory,output_filename):
             'sst_gradient_y':sst_gradient_y
         }
     )
-
+    herald("dataset created successfully")
     # set attributes for the gradient variables
     sst_gradient_ds['sst_gradient_x'].attrs = {
         'units': 'K/m',
@@ -64,7 +75,7 @@ def sst_gradient_to_da(sst,output_directory,output_filename):
         'references': '[ADD REFERENCES]'
     }
     
-
+    herald("new attributes assigned successfully")
     # Ensure the output directory exists
     os.makedirs(output_directory, exist_ok=True)
 
@@ -72,6 +83,6 @@ def sst_gradient_to_da(sst,output_directory,output_filename):
     file_path = os.path.join(output_directory, output_filename)
     sst_gradient_ds.to_netcdf(file_path)
 
-    herald(f"Data successfully written to {file_path}")
+    herald(f"data successfully written to {file_path}")
 
 
