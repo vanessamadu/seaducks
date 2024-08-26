33d9cd87bc152bfb05005fb9dd5005417fdeda23
Vanessa Madu <vm2218@ic.ac.uk>
2024-08-26 12:49:06 +0100

    removing 5-point stencil

--- seaducks/data_processing/derived_quantities.py
+++ seaducks/data_processing/derived_quantities.py
@@ -32,20 +32,21 @@ def sst_gradient_pointwise(sst_array: xr.DataArray, coord_str: str, time_val: np
     '''
     
     # metadata
-    h = 0.05 # degrees
+    grid_space = 0.05 # degrees
     earth_radius = 6371 # km
     
 
     lat_val_str,lon_val_str = coord_str
 
     # find sst values near coord
-    lat_neighbours = [format_coordinates(float(lat_val_str)+ii*h) for ii in np.arange(-2,3,1)]
-    lon_neighbours = [format_coordinates(float(lon_val_str)+jj*h) for jj in np.arange(-2,3,1)]
+    lat_neighbours = [format_coordinates(float(lat_val_str)+ii*grid_space) for ii in np.arange(-1,2,1)]
+    lon_neighbours = [format_coordinates(float(lon_val_str)+jj*grid_space) for jj in np.arange(-1,2,1)]
     sst_x_neighbours = [float(sst_array.sel(time=time_val,latitude=lat_val, longitude=lon_val_str).values) if 0 < float(lat_val) < 60 else np.nan for lat_val in lat_neighbours]
     sst_y_neighbours = [float(sst_array.sel(time=time_val,latitude=lat_val_str, longitude=lon_val).values) if -83< float(lon_val)<-40 else np.nan for lon_val in lon_neighbours]
-    h = np.deg2rad(h)*earth_radius # convert to km
-    
-    return (diff1d(sst_x_neighbours,h)[2],diff1d(sst_y_neighbours,h)[2]) # return centre values
+    h_lat = grid_space*earth_radius # convert to km
+    h_lon = grid_space*earth_radius*np.cos(np.deg2rad(float(lat_val_str))) # convert to km
+
+    return (diff1d(sst_x_neighbours,h_lon)[2],diff1d(sst_y_neighbours,h_lat)[2]) # return centre values
 
 def interpolate_sst_gradient(drifter_lat: float, drifter_lon: float, time_val:np.datetime64, sst_array:xr.DataArray,corners:np.ndarray) -> tuple:
     '''
