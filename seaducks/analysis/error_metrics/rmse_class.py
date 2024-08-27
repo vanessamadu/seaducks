6c9b2eab056d29c4f8087c5452b80b461055cd64
Vanessa Madu <vm2218@ic.ac.uk>
2024-08-27 09:35:21 +0100

    initial commit and move files

---
+++ seaducks/analysis/error_metrics/rmse_class.py
@@ -0,0 +1,30 @@
+import numpy as np
+from error_metrics import Error
+
+class RMSE(Error):
+    def __init__(self,predictions,observations):
+        self._predictions = np.array(predictions).flatten()
+        self._observations = np.array(observations).flatten()
+    
+    @property
+    def predictions(self):
+        return self._predictions
+    @property
+    def observations(self):
+        return self._observations
+    
+    @staticmethod
+    def rmse(residuals_arr: np.ndarray) -> float:
+        return np.sqrt(np.mean(np.square(residuals_arr)))  
+
+    ## Error-inherited properties
+    @property
+    def error_type(self):
+        return "rmse"
+    @property
+    def error(self):
+        return __class__.rmse(self.residuals)
+    
+
+        
+    
