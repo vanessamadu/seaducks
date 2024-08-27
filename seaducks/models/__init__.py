91822afe9569cf6960260b3419ad59093a6e21d3
Vanessa Madu <vm2218@ic.ac.uk>
2024-08-27 14:35:00 +0100

    finish docstring

--- seaducks/models/__init__.py
+++ seaducks/models/__init__.py
@@ -1,6 +1,3 @@
-'description: hierarchical model framework for the construction of my ocean drifter velocity model'
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%% SET UP %%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 ##### import packages #####
 import numpy as np
 import pandas as pd
@@ -8,14 +5,43 @@ from abc import ABC, abstractmethod
 
 class Model(ABC):
     '''
-    this class will be the parent class of all ocean models that we will be using
-    and it will define attributes and methods common to all specific model classes.
+    A parent class for the ocean surface current models featured in SeaDucks.
+    
+    Attributes
+    ----------
+    data: pd.DataFrame
+        A dataset containing drifter data and satellite (derived) data interpolated to drifter
+        positions.
     '''
     def __init__(self,data: pd.DataFrame):
+        ''''
+        Initialises Model with data.
+
+        Parameters
+        ----------
+        data: pd.DataFrame
+            The data to assign to the instance
+        '''
         self._data = data
 
     @staticmethod
     def check_coordinates(lon:float, lat:float):
+        '''
+        Checks that coordinates are valid.
+
+        Parameters
+        ----------
+        lon: float
+            longitude 
+        lat: float
+            latitude
+
+        Raises
+        ------
+        ValueError: when
+            - lat or lon are not floats
+            - |lat|>90 and |lon|>180
+        '''
         limits = {"lat":90.,"lon":180.}
         values = {"lat":lat,"lon":lon}
 
@@ -30,10 +56,26 @@ class Model(ABC):
                 
     @property
     def data(self):
+        '''
+        [immutable property] Gets value of data. 
+
+        Returns
+        -------
+        pd.DataFrame:
+            The data associated with the instance.
+        '''
         return self._data
     
     @property
     def observations(self):
+        '''
+        [immutable property] Gets drifter velocities.
+
+        Returns
+        -------
+        np.array:
+        The values of (u,v) for each drifter.
+        '''
         return np.array(self.data[["u","v"]])
     
     @abstractmethod
