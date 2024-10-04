5cb48ab2a39c311224f8210784ced0aa4df8262e
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-10-04 10:19:07 +0100

    add new return variables

--- seaducks/config.py
+++ seaducks/config.py
@@ -1,7 +1,7 @@
 # seaducks/config.py
 config = {
     'random_state': 42, # random seed
-    'return_variables' : ['lon', 'lat', 'id', 'time', 'drogue', 'u', 'v', 'Wx', 'Wy', 'Tx', 'Ty','u_filtered', 'v_filtered', 'Wx_filtered', 'Wy_filtered', 'Tx_filtered', 'Ty_filtered',
+    'return_variables' : ['lon', 'lat', 'id', 'time', 'drogue', 'u', 'v', 'Wx', 'Wy', 'Tx', 'Ty','u_raw', 'v_raw', 'Wx_raw', 'Wy_raw', 'Tx_raw', 'Ty_raw',
        'err_Wx', 'err_Wy', 'u_av', 'v_av', 'adt', 'sla', 'ugos', 'vgos',
-       'adt_err', 'lon_var', 'lat_var']
+       'adt_err', 'lon_var', 'lat_var','sst_x_derivative','sst_y_derivative']
 }
