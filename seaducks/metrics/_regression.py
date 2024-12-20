e2e82b8efd3217f23a3a5a6394f816ba6ebcd94c
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-20 10:35:42 +0000

    add mae

--- seaducks/metrics/_regression.py
+++ seaducks/metrics/_regression.py
@@ -2,9 +2,30 @@
 # Author: Vanessa Madu
 
 import sklearn.metrics as skm
+from seaducks.metrics._metrics_cl import Metric
+
+# typing
 from typing import Literal
 from pyvista import ArrayLike, MatrixLike
-from seaducks.metrics._metrics_cl import Metric
+from numpy import ndarray
+
+class MAE(Metric):
+    
+    def __init__(self, y_true: MatrixLike | ArrayLike, y_pred: MatrixLike | ArrayLike, *,
+                 sample_weight: ArrayLike | None, multioutput: ArrayLike | Literal['raw_values', 'uniform_average'] = "uniform_average"):
+        
+        super().__init__(y_true,y_pred)
+
+        self.sample_weight = sample_weight
+        self.multioutput = multioutput
+        self.string_name = 'mae'
+
+    def mae(self) -> (float | ndarray):
+        return skm.mean_absolute_error(self.y_true, self.y_pred, 
+                                           sample_weight=self.sample_weight, multioutput=self.multioutput)
+
+class MAAO(Metric):
+    pass
 
 class RMSE(Metric):
 
@@ -17,15 +38,9 @@ class RMSE(Metric):
         self.multioutput = multioutput
         self.string_name = 'rmse'
     
-    def rmse(self):
+    def rmse(self) -> (float | ndarray):
         return skm.root_mean_squared_error(self.y_true, self.y_pred, 
                                            sample_weight=self.sample_weight, multioutput=self.multioutput)
 
 class RMSLE(Metric):
-    pass
-
-class MAE(Metric):
-    pass
-
-class MAAO(Metric):
     pass
