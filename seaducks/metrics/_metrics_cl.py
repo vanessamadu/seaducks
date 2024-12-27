0e9479bc3f1dbb9f46dc4ff5ae6dd3859ca4b4ff
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-27 17:25:38 +0000

    updates to scores and metrics class

--- seaducks/metrics/_metrics_cl.py
+++ seaducks/metrics/_metrics_cl.py
@@ -2,6 +2,7 @@
 # Author: Vanessa Madu
 
 from pyvista import ArrayLike, MatrixLike
+from ngboost.distns.multivariate_normal import get_chol_factor as get_L_matrix
 
 class Metric():
     
@@ -14,3 +15,16 @@ class Metric():
         self.sample_weight = None
         self.valid_loss = None
         self.valid_risk = None
+
+class MVNScore():
+
+    def __init__(self,y_true: MatrixLike | ArrayLike, pred_params: ArrayLike | MatrixLike):
+        
+        self.y_true = y_true
+        self.pred_params = pred_params
+
+        self.mvn_dim = y_true.shape[0]
+        
+        self.loc = pred_params[:self.mvn_dim,:]
+        self.chol_factors = pred_params[self.mvn_dim:,:]
+        self.L = get_L_matrix(self.chol_factors)
