510bfdb5e8264e63382bd71824323357bd31cb8f
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-27 13:04:59 +0000

    add chi2

--- seaducks/metrics/_fit_scores.py
+++ seaducks/metrics/_fit_scores.py
@@ -3,6 +3,7 @@
 
 import sklearn.metrics as skm
 import scipy.stats as stats
+import numpy as np
 from seaducks.metrics._metrics_cl import Metric
 
 # typing
@@ -27,4 +28,31 @@ class R2_score(Metric):
                             sample_weight=self.sample_weight, multioutput=self.multioutput, force_finite=self.force_finite)
     
 class Chi2_statistic(Metric):
-    pass
+    
+    def __init__(self, y_true: ArrayLike | MatrixLike, y_pred: ArrayLike | MatrixLike, *,
+                 sample_weight: ArrayLike | None, multioutput: ArrayLike | Literal['raw_values', 'uniform_average'] = "raw_values", axis: int=0):
+        super().__init__(y_true, y_pred)
+
+        self.multioutput = multioutput
+        self.string_name = 'chi2_stat'
+        self.sample_weight = sample_weight
+        self.ddof = len(y_true)-1
+        self.axis = axis
+
+    def chi2_stat(self, return_p_value = False) -> (float | ndarray):
+        
+        chi2, p_value = stats.chisquare(self.y_true, 
+        f_exp = self.y_pred, ddof = self.ddof, axis = self.axis)
+
+        if return_p_value:
+            if self.multioutput == 'raw_values':
+                return chi2, p_value
+            elif self.multioutput == 'uniform_average':
+                return np.average(chi2, weights = self.sample_weight), 
+                np.average(p_value, weights = self.sample_weight)
+        else: 
+            if self.multioutput == 'raw_values':
+                return chi2
+            elif self.multioutput == 'uniform_average':
+                return np.average(chi2, weights = self.sample_weight)
+
