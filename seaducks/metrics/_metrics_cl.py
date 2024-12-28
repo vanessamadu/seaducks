15a14ec0cb8d3fa8aae121c011f6c6071dea3bb1
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-28 14:59:41 +0000

    adjust Metric to include property decorators

--- seaducks/metrics/_metrics_cl.py
+++ seaducks/metrics/_metrics_cl.py
@@ -1,20 +1,42 @@
 ''' Class for all regression and fit score metrics'''
 # Author: Vanessa Madu
 
-from pyvista import ArrayLike, MatrixLike
 from ngboost.distns.multivariate_normal import get_chol_factor as get_L_matrix
+# typing
+from pyvista import ArrayLike, MatrixLike
+from typing import Literal
 
 class Metric():
     
-    def __init__(self, y_true: MatrixLike | ArrayLike, y_pred: MatrixLike | ArrayLike):
+    def __init__(self, y_true: MatrixLike | ArrayLike, y_pred: MatrixLike | ArrayLike, *,
+                 sample_weight: ArrayLike | None, multioutput: ArrayLike | Literal['raw_values', 'uniform_average'] = "uniform_average"):
+        """_summary_
+
+        Args:
+            y_true (MatrixLike | ArrayLike): _description_
+            y_pred (MatrixLike | ArrayLike): _description_
+        """
         self.y_true = y_true
         self.y_pred = y_pred
-
-        self.multioutput = None
+        # keyword arguments
+        self.multioutput = multioutput
+        self.sample_weight = sample_weight
+        # immutable properties
         self.string_name = None
-        self.sample_weight = None
         self.valid_loss = None
         self.valid_risk = None
+    
+    @property
+    def string_name():
+        pass
+
+    @property
+    def valid_loss():
+        pass
+
+    @property
+    def valid_risk():
+        pass
 
 class MVNScore():
 
