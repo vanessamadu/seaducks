09e2b825a1122df4acc42cb3e58fb2969ca4b7b2
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-28 15:14:19 +0000

    add properties to MVNScore

--- seaducks/metrics/_metrics_cl.py
+++ seaducks/metrics/_metrics_cl.py
@@ -25,11 +25,9 @@ class Metric():
     @property
     def string_name(self):
         pass
-
     @property
     def valid_loss(self):
         pass
-
     @property
     def valid_risk(self):
         pass
@@ -43,11 +41,22 @@ class MVNScore():
             y_true (MatrixLike | ArrayLike): _description_
             pred_params (ArrayLike | MatrixLike): _description_
         """
-        
         self.y_true = y_true
-        self.pred_params = pred_params
-        # read-only properties
-        self._mvn_dim = y_true.shape[0]
-        self._loc = pred_params[:self.mvn_dim,:]
-        self._chol_factors = pred_params[self.mvn_dim:,:]
+        self.pred_params = pred_params 
+    # read-only properties
+    @property
+    def mvn_dim(self):
+        self._mvn_dim = self.y_true.shape[0]
+        return self._mvn_dim
+    @property
+    def loc(self):
+        self._loc = self.pred_params[:self.mvn_dim,:]
+        return self._loc
+    @property
+    def chol_factors(self):
+        self._chol_factors = self.pred_params[self.mvn_dim:,:]
+        return self._chol_factors
+    @property
+    def L(self):
         self._L = get_L_matrix(self.chol_factors)
+        return self._L
