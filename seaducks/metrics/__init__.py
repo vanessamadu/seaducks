f3ff7ecf90bf8270d2c1dc0fa207a3f94ddfde76
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-28 18:05:15 +0000

    importing all metrics

--- seaducks/metrics/__init__.py
+++ seaducks/metrics/__init__.py
@@ -1,8 +1,11 @@
 ''' Score functions and performance metrics '''
 # Author: Vanessa Madu
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname('seaducks/metrics'), '..')))
 
-from _fit_scores import R2_Score, Chi2_Statistic, Prediction_Region
-from _regression import RMSE, RMSLE, MAE, MAAO
+from seaducks.metrics._fit_scores import R2_Score, Chi2_Statistic, Prediction_Region
+from seaducks.metrics._regression import RMSE, RMSLE, MAE, MAAO
 
 __all__ = ['chi2_stat',
            'mae',
