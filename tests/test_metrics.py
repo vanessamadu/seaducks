c42119f68ffcb59f5d86deedbc00c96952e8bb14
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-28 17:14:12 +0000

    add regression skeleton

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,7 @@
 import pytest
 
-@pytest.mark.regression
+"""
+Parameterize example
 @pytest.mark.parametrize("true",[
     1+1 == 2,
     True,
@@ -8,3 +9,34 @@ import pytest
 ])
 def test_test(true):
     assert true
+"""
+# -------------- Regression Metrics -------------- #
+
+@pytest.mark.regression
+@pytest.mark.parametrize("mae",[
+
+])
+def test_mae(mae):
+    pass
+
+@pytest.mark.regression
+@pytest.mark.parametrize("maao",[
+
+])
+def test_maao(maao):
+    pass
+
+@pytest.mark.regression
+@pytest.mark.parametrize("rmse",[
+
+])
+def test_rmse(rmse):
+    pass
+
+@pytest.mark.regression
+@pytest.mark.parametrize("rmsle",[
+
+])
+def test_rmsle(rmsle):
+    pass
+
