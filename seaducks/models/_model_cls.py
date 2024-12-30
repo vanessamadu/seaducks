60a8a39f55208ca128121a0aff130dc5102f3f9d
vanessamadu <vanessa.madu18@imperial.ac.uk>
2024-12-30 17:43:58 +0000

    new model class

--- seaducks/models/_model_cls.py
+++ seaducks/models/_model_cls.py
@@ -1,7 +1,18 @@
 import pickle
 
 class Model:
-    def __init__(self):
+    def __init__(self, random_state: int = 42):
+        self.random_state = random_state
+
+    # read-only properties
+    @property
+    def string_name(self):
+        pass
+    @property
+    def model(self):
+        pass
+    
+    def fit(self):
         pass
 
     def save_model(self, file_name):
