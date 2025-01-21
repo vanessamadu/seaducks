fec58555687c47183cec7cc83bf62ef0fed593e5
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-01-21 10:19:14 +0000

    correction

--- experiments/hpc_scripts/config_search.py
+++ experiments/hpc_scripts/config_search.py
@@ -12,15 +12,15 @@ def model_config_verbose(experiment_ID,model_filepath):
     print('\n Hyperparameter Values')
     print('---------------------------')
 
-    print(f'eta: {config_id[0]}')
-    print(f'min. data per leaf: {config_id[1]}')
-    print(f'max. leaves per node: {config_id[2]}')
+    print(f'eta: {config[0]}')
+    print(f'min. data per leaf: {config[1]}')
+    print(f'max. leaves per node: {config[2]}')
 
     print('\n Data Information')
     print('---------------------------')
 
-    print(f'Uses SST gradient: {config_id[3]}')
-    print(f'Uses velocities in polar form: {config_id[4]}')
+    print(f'Uses SST gradient: {config[3]}')
+    print(f'Uses velocities in polar form: {config[4]}')
 
     return config
 
