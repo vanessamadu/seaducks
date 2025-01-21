6540e315ca2ea4bc1f7accec13554a8daf5f6670
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-01-21 15:18:33 +0000

    update grid search

--- experiments/hpc_scripts/config_search.py
+++ experiments/hpc_scripts/config_search.py
@@ -1,18 +1,16 @@
 import pickle
 import numpy as np
  
-def model_config(experiment_ID,model_filepath,*,
+def model_config(config_id,model_filepath,*,
                          verbose=False):
     with open(model_filepath,'rb') as pickle_file:
         model_configs = pickle.load(pickle_file)
-    config_id = int(np.floor(experiment_ID/10))
     config = model_configs[config_id]
 
     if verbose:
         # ------------ print useful information -------------- #
-        print(f'\nexperiment ID: {experiment_ID}')
-        print(f'config ID: {config_id}')
-        print(f'replication: {(experiment_ID-1)%10}')
+        print(f'\nconfig ID: {config_id}')
+
         print('\n Hyperparameter Values')
         print('---------------------------')
 
