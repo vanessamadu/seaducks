e394ac9992bc7f3cbc25d40a45a9eccce2054ba6
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-01-21 10:32:01 +0000

    make verbose optional

--- experiments/hpc_scripts/config_search.py
+++ experiments/hpc_scripts/config_search.py
@@ -1,28 +1,30 @@
 import pickle
 import numpy as np
  
-def model_config_verbose(experiment_ID,model_filepath):
+def model_config(experiment_ID,model_filepath,*,
+                         verbose=False):
     with open(model_filepath,'rb') as pickle_file:
         model_configs = pickle.load(pickle_file)
     config_id = int(np.floor(experiment_ID/10))
     config = model_configs[config_id]
 
-    # ------------ print useful information -------------- #
-    print(f'\nexperiment ID: {experiment_ID}')
-    print(f'config ID: {config_id}')
-    print(f'replication: {(experiment_ID-1)%10}')
-    print('\n Hyperparameter Values')
-    print('---------------------------')
+    if verbose:
+        # ------------ print useful information -------------- #
+        print(f'\nexperiment ID: {experiment_ID}')
+        print(f'config ID: {config_id}')
+        print(f'replication: {(experiment_ID-1)%10}')
+        print('\n Hyperparameter Values')
+        print('---------------------------')
 
-    print(f'learning rate: {config[0]}')
-    print(f'min. data per leaf: {config[1]}')
-    print(f'max. leaves per node: {config[2]}')
+        print(f'learning rate: {config[0]}')
+        print(f'min. data per leaf: {config[1]}')
+        print(f'max. leaves per node: {config[2]}')
 
-    print('\n Data Information')
-    print('---------------------------')
+        print('\n Data Information')
+        print('---------------------------')
 
-    print(f'Uses SST gradient: {config[3]}')
-    print(f'Uses velocities in polar form: {config[4]}')
+        print(f'Uses SST gradient: {config[3]}')
+        print(f'Uses velocities in polar form: {config[4]}')
 
     return config
 
