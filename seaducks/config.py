096e78a2e8ba3b7739dc67753cfc50a2398831e7
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-01-22 10:43:18 +0000

    add more seeds

--- seaducks/config.py
+++ seaducks/config.py
@@ -1,7 +1,13 @@
 # seaducks/config.py
 import numpy as np
+import pickle
+
+rootdir = r'/rds/general/user/vm2218/home/phd-project1/SeaDucks/seaducks/'
+with open(r'81_10_9_seed_indices.p', 'rb') as pickle_file:
+    seeds = pickle.load(pickle_file)
+
 config = {
-    '81-10-9_random_states': [np.random.seed(val) for val in [32327, 45747, 52776, 63012, 74898, 95853, 96672, 108808, 113639, 126432]], 
+    '81-10-9_random_states': [np.random.seed(val) for val in seeds], 
     'return_variables' : ['lon', 'lat', 'id', 'time', 'drogue', 'u', 'v', 'Wx', 'Wy', 'Tx', 'Ty','u_raw', 'v_raw', 'Wx_raw', 'Wy_raw', 'Tx_raw', 'Ty_raw',
        'err_Wx', 'err_Wy', 'u_av', 'v_av', 'adt', 'sla', 'ugos', 'vgos',
        'adt_err', 'lon_var', 'lat_var','sst_x_derivative','sst_y_derivative']
