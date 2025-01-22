ce6be9f32911959f7a2160fcd568512b6582a9e3
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-01-22 11:12:06 +0000

    update to save

--- scripts/run_find_seeds.py
+++ scripts/run_find_seeds.py
@@ -1,12 +1,17 @@
 import os
 import sys
 import pandas as pd
+import pickle
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname('seaducks/model_selection'), '..')))
 from seaducks.model_selection import nominal_cluster_sampling
 
 if __name__ == "__main__":
     # load data
-    path_to_data = r'C:\Users\vm2218\OneDrive - Imperial College London\PhD Project\seaducks\data\filtered_nao_drifters_with_sst_gradient.h5'
+    path_to_data = r'C:\Users\vm2218\OneDrive - Imperial College London\PhD Project\seaducks\data\complete_filtered_nao_drifter_dataset.h5'
     data = pd.read_hdf(path_to_data)
+    seeds = nominal_cluster_sampling(data, num_seeds=100)
+    print(seeds)
 
-    print(nominal_cluster_sampling(data, num_seeds=10))
+    file_name = '81_10_9_seed_indices'
+    filehandler = open(f"{file_name}.p","wb")
+    pickle.dump(seeds,filehandler)
