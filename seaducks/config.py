fe5f497c10a684cd1b546c732dc3f8370e28ec1d
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-02-03 14:16:54 +0000

    remove random states

--- seaducks/config.py
+++ seaducks/config.py
@@ -8,7 +8,6 @@ with open(fr'{rootdir}81_10_9_seed_indices.p', 'rb') as pickle_file:
 
 config = {
     '81-10-9_random_seeds':seeds,
-    '81-10-9_random_states': [np.random.seed(val) for val in seeds], 
     'return_variables' : ['lon', 'lat', 'id', 'time', 'drogue', 'u', 'v', 'Wx', 'Wy', 'Tx', 'Ty','u_raw', 'v_raw', 'Wx_raw', 'Wy_raw', 'Tx_raw', 'Ty_raw',
        'err_Wx', 'err_Wy', 'u_av', 'v_av', 'adt', 'sla', 'ugos', 'vgos',
        'adt_err', 'lon_var', 'lat_var','sst_x_derivative','sst_y_derivative']
