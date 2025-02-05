74f7d6aa53e3db22daea0b5eea0d232721ccafbe
vanessamadu <vanessa.madu18@imperial.ac.uk>
2025-02-05 11:03:25 +0000

    typo

--- generate_bash_scripts/pbs_scripts.py
+++ generate_bash_scripts/pbs_scripts.py
@@ -14,7 +14,7 @@ def run_experiment_for_all_configs(walltime:str,select:int,ncpus:int,mem:int,lea
         filename += f'_{lr}'
     with open (f'{filename}.pbs', 'w') as rsh:
         rsh.write(f'''\
-#! /bin/bash
+#!/bin/bash
 #PBS -l walltime={walltime}
 #PBS -l select={select}:ncpus={ncpus}:mem={mem}gb
 #PBS -J {array_indices}
@@ -26,13 +26,13 @@ mamba activate SeaDucks
 
 cd $PBS_O_WORKDIR
 
+python experiments/hpc_scripts/fit_mvn_ngboost.py ${{PBS_ARRAY_INDEX}} > experiment_${{PBS_ARRAY_INDEX}}_early_stopping_{early_stopping}_logs
+
 # create directories that don't exist
 mkdir -p {root_dir}/early_stopping_{early_stopping}/{date}/experiment_logs
 mkdir -p {root_dir}/early_stopping_{early_stopping}/{date}/fit_models
 mkdir -p {root_dir}/early_stopping_{early_stopping}/{date}/model_test_data
 mkdir -p {root_dir}/early_stopping_{early_stopping}/{date}/out
-
-python experiments/hpc_scripts/fit_mvn_ngboost.py ${{PBS_ARRAY_INDEX}} > experiment_${{PBS_ARRAY_INDEX}}_early_stopping_{early_stopping}_logs
 
 # copy files back
 mv experiment_${{PBS_ARRAY_INDEX}}_date_{date}_early_stopping_{early_stopping}_logs {root_dir}/early_stopping_{early_stopping}/{date}/experiment_logs
